466b989b7c05851da44f4640099a604f8697a881
natellerman <natellerman@gmail.com>
2025-04-07 18:36:44 -0400

    4/7

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -14,18 +14,4 @@ public class PlayerMovement : MonoBehaviour
         movement.x = input * speed * Time.deltaTime;
         transform.Translate(movement);
     }
-public class PlayerController : MonoBehaviour
-{
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-}
 }
