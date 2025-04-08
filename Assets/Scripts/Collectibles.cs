ad22bd4976b4e4cd898ee76175806a428957733b
natellerman <natellerman@gmail.com>
2025-04-08 19:24:29 -0400

    Collectible script

---
+++ Assets/Scripts/Collectibles.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class Collectible : MonoBehaviour
+{
+    public int points = 10;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Collect(other.gameObject);
+        }
+    }
+
+    private void Collect(GameObject player)
+    {
+        Debug.Log("Collected! Points: " + points);
+
+        Destroy(gameObject);
+    }
+}
