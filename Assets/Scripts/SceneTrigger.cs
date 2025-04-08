4ee4d24dc1ccac365870271d06113d4a730023f9
natellerman <natellerman@gmail.com>
2025-04-08 19:11:25 -0400

    Fixed SceneTrigger script

--- Assets/Scripts/SceneTrigger.cs
+++ Assets/Scripts/SceneTrigger.cs
@@ -1,11 +1,11 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class SceneChangeTrigger : MonoBehaviour
+public class SceneTrigger : MonoBehaviour
 {
     public string sceneToLoad;
 
-    private void OnTriggerStay2D(Collider2D other)
+    private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Player"))
         {
