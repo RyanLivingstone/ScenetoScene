92c5b3a6197ceb48a4d817e854df19945e06b603
natellerman <natellerman@gmail.com>
2025-04-09 20:20:05 -0400

    Fire

--- Assets/Scripts/WinScreen.cs
+++ Assets/Scripts/WinScreen.cs
@@ -1,23 +1,16 @@
 using UnityEngine;
-using UnityEngine.UI;  // Required for accessing UI elements
+using UnityEngine.UI;
 
 public class WinScreen : MonoBehaviour
 {
-    // Reference to the You Win Canvas or Panel
     public GameObject winScreen;
 
-
-    // Detect player collision with the trophy
     private void OnTriggerEnter2D(Collider2D other)
     {
-        // Check if the object colliding is the player
         if (other.CompareTag("Player"))
         {
-            // Show the You Win screen
             winScreen.SetActive(true);
-
-            // Optionally, disable the player movement or other gameplay elements
-            Time.timeScale = 0f; // Stops the game (pause)
+            Time.timeScale = 0f;
         }
     }
 }
